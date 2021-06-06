3700fe8d452e92d5b9819a49ec121e8f1be557d4
Aswebilius <81036333+Aswebilius@users.noreply.github.com>
2021-06-05 21:35:34 -0400

    API Key Accepted, Results Produced

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
-var apiKey = '1e05fe3a438cffe5221efeafd2cf9863'
+var api = {
+    key: "1e05fe3a438cffe5221efeafd2cf9863",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
 
-fetch()
+const searchBox = document.querySelector('.search-box');
+
+searchBox.addEventListener('keypress', setQuery);
+
+function setQuery(evt){
+    if (evt.keyCode == 13) {
+    getSearch(searchBox.value);
+    console.log(searchBox.value);
+    }
+}
+
+function getSearch (query){
+    fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displaySearch);
+}
+
+function displaySearch(weather) {
+    console.log(weather);
+}
